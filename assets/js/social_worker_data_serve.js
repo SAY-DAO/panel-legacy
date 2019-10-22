de12752b8869bf6379e53a9f0bb7a5fc5152130f
elham <elham.ne94@gmail.com>
2019-10-22 19:47:38 +0330

    social worker edit

--- assets/js/social_worker_data_serve.js
+++ assets/js/social_worker_data_serve.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var keys = ['id' , 'generatedCode' , 'firstName' , 'lastName' , 'userName' , 'id_type' , 'id_ngo' , 'birthCertificateNumber' , 'city' , 'country' , 'idNumber', 'idCardUrl' , 'passportNumber' , 'passportUrl' , 'gender' , 'birthDate' , 'phoneNumber' , 'emergencyPhoneNumber' , 'emailAddress' , 'telegramId' , 'postalAddress' , 'avatarUrl' , 'currentChildCount', 'currentNeedCount' , 'bankAccountNumber' , 'bankAccountShebaNumber' , 'bankAccountCardNumber' , 'registerDate' , 'lastLoginDate' , 'lastUpdateDate']
+    var keys = ['id' , 'generatedCode' , 'firstName' , 'lastName' , 'userName' , 'avatarUrl' , 'id_type' , 'id_ngo' , 'birthCertificateNumber' , 'city' , 'country' , 'idNumber', 'idCardUrl' , 'passportNumber' , 'passportUrl' , 'gender' , 'birthDate' , 'phoneNumber' , 'emergencyPhoneNumber' , 'emailAddress' , 'telegramId' , 'postalAddress' , 'currentChildCount', 'currentNeedCount' , 'bankAccountNumber' , 'bankAccountShebaNumber' , 'bankAccountCardNumber' , 'registerDate' , 'lastLoginDate' , 'lastUpdateDate']
     $.ajax({
         url: SAYApiUrl + '/socialWorker/all',
         method: 'GET',
@@ -120,70 +120,57 @@ $(document).ready(function(){
         var bankAccountNumber = $('#social_worker_bank_account_number').val();
         var bankAccountShebaNumber = $('#social_worker_bank_account_sheba_number').val();
         var bankAccountCardNumber = $('#social_worker_bank_account_card_number').val();
+        var idCardUrl = $('#social_worker_id_card')[0].files[0];
+        var passportUrl = $('#social_worker_passport')[0].files[0];
+        var avatarUrl = $('#social_worker_avatar')[0].files[0];
 
         var form_data = new FormData();
-        if($('#social_worker_id_card')[0].files[0]){        
-            form_data.append('idCardUrl', $('#social_worker_id_card')[0].files[0]);
+        if(idCardUrl){        
+            form_data.append('idCardUrl', idCardUrl);
         }
-
-        if($('#social_worker_passport')[0].files[0]){
-            form_data.append('passportUrl', $('#social_worker_passport')[0].files[0]);
+        if(passportUrl){
+            form_data.append('passportUrl', passportUrl);
         }
-
-        form_data.append('avatarUrl', $('#social_worker_avatar')[0].files[0]);
-
         if(firstName){
             form_data.append('firstName', firstName);
         }
-
-        form_data.append('lastName', lastName);
-        form_data.append('id_type', id_type);
-        form_data.append('id_ngo', id_ngo);
-
         if(birthCertificateNumber){        
             form_data.append('birthCertificateNumber', birthCertificateNumber);
         }
-
         if(country){        
             form_data.append('country', country);
         }
-
         if(city){        
             form_data.append('city', city);
         }
-
-        form_data.append('idNumber', idNumber);
-
         if(passportNumber){        
             form_data.append('passportNumber', passportNumber);
         }
-
-        form_data.append('gender', gender);
-
         if(birthDate){        
             form_data.append('birthDate', birthDate);
         }
-
+        if(postalAddress){        
+            form_data.append('postalAddress', postalAddress);
+        }
+        if(bankAccountNumber){        
+            form_data.append('bankAccountNumber', bankAccountNumber);
+        }
+        if(bankAccountShebaNumber){        
+            form_data.append('bankAccountShebaNumber', bankAccountShebaNumber);
+        }
+        if(bankAccountCardNumber){        
+            form_data.append('bankAccountCardNumber', bankAccountCardNumber);
+        }
+        form_data.append('avatarUrl', avatarUrl);
+        form_data.append('lastName', lastName);
+        form_data.append('id_type', id_type);
+        form_data.append('id_ngo', id_ngo);
+        form_data.append('idNumber', idNumber);
+        form_data.append('gender', gender);
         form_data.append('phoneNumber', phoneNumber);
         form_data.append('emergencyPhoneNumber', emergencyPhoneNumber);
         form_data.append('emailAddress', emailAddress);
         form_data.append('telegramId', telegramId);
-
-        if(postalAddress){        
-            form_data.append('postalAddress', postalAddress);
-        }
-
-        if(bankAccountNumber){        
-            form_data.append('bankAccountNumber', bankAccountNumber);
-        }
-
-        if(bankAccountShebaNumber){        
-            form_data.append('bankAccountShebaNumber', bankAccountShebaNumber);
-        }
-
-        if(bankAccountCardNumber){        
-            form_data.append('bankAccountCardNumber', bankAccountCardNumber);
-        }
 
         console.log(form_data);
 
@@ -197,14 +184,20 @@ $(document).ready(function(){
             processData: false,
             contentType: false,
             data: form_data,
+            beforeSend: function(){
+                return confirm("You are about to add new user.\nAre you sure?");
+            },
             success: function(data) {
                 console.log(data);
-                alert('Success');
+                // alert("Success\n" + JSON.stringify(data['message']));
+                alert("Success\n" + data.responseJSON.message);                
                 location.reload();
             },
             error: function(data) {
-                console.log(data);
-                alert('Error!');
+                bootbox.alert({
+                    title: "Error!",
+                    message: data.responseJSON.message,
+                });
             }
         })
         
@@ -250,6 +243,7 @@ $(document).ready(function(){
                 $('#social_worker_bank_account_sheba_number').val(data['bankAccountShebaNumber']);
                 $('#social_worker_bank_account_card_number').val(data['bankAccountCardNumber']);
                 $('#social_worker_current_username').val(data['userName']);
+                $('#social_worker_current_password').val(data['password']);
                 $('#social_worker_current_child_count').val(data['currentChildCount']);
                 $('#social_worker_current_need_count').val(data['currentNeedCount']);
 
@@ -259,94 +253,141 @@ $(document).ready(function(){
             }
         })
 
-        // $('#editNeedData').on('click' , function(e) {
-        //     e.preventDefault();
+        $('#editSocialWorkerData').on('click' , function(e) {
+            e.preventDefault();
 
-        //     // getting data from html form
+            // getting data from html form
 
-        //     var name = $('#need_name').val();
-        //     var imageUrl = $('#need_icon')[0].files[0];
-        //     var category = $('#need_category').val();
-        //     var cost = $('#need_cost').val();
-        //     var description = $('#need_description').val();
-        //     var descriptionSummary = $('#need_description_summary').val();
-        //     var type = $('#need_type').val();
-        //     var doing_duration = $('#need_doing_duration').val();
-        //     var isUrgent = '';
-        //     if($('#is_urgent').is(":checked")){
-        //         isUrgent = true;
-        //     }else{
-        //         isUrgent = false;
-        //     }
-        //     var affiliateLinkUrl = $('#affiliate_link').val();
-        //     var receipts = $('#need_receipts')[0].files[0];
+            var firstName = $('#social_worker_first_name').val();
+            var lastName = $('#social_worker_last_name').val();
+            var id_type = $('#social_worker_type').val();
+            var id_ngo = $('#social_worker_ngo').val();
+            var birthCertificateNumber = $('#social_worker_certificate_number').val();
+            var country = $('#social_worker_country').val();
+            var city = $('#social_worker_city').val();
+            var idNumber = $('#social_worker_id_number').val();
+            var passportNumber = $('#social_worker_passport_number').val();
+            var gender = $('#social_worker_gender').val();
+            var birthDate = $('#social_worker_birth_date').val();
+            var phoneNumber = $('#social_worker_phone_number').val();
+            var emergencyPhoneNumber = $('#social_worker_emergency_phone_number').val();
+            var emailAddress = $('#social_worker_email_address').val();
+            var telegramId = $('#social_worker_telegram_id').val();
+            var postalAddress = $('#social_worker_postal_address').val();
+            var userName = $('#social_worker_current_username').val();
+            var password = $('#social_worker_current_password').val();
+            var bankAccountNumber = $('#social_worker_bank_account_number').val();
+            var bankAccountShebaNumber = $('#social_worker_bank_account_sheba_number').val();
+            var bankAccountCardNumber = $('#social_worker_bank_account_card_number').val();
+            var idCardUrl = $('#social_worker_id_card')[0].files[0];
+            var passportUrl = $('#social_worker_passport')[0].files[0];
+            var avatarUrl = $('#social_worker_avatar')[0].files[0];
 
-        //     // append datas to a Form Data
-        //     var form_data = new FormData();
-        //     if(imageUrl) {
-        //         form_data.append('imageUrl', imageUrl);
-        //     }
-        //     if(name) {
-        //         form_data.append('name', name);
-        //     }
-        //     if(category) {
-        //         form_data.append('category', category);
-        //     }
-        //     if(cost) {
-        //         form_data.append('cost', cost);
-        //     }
-        //     if(description) {
-        //         form_data.append('description', description);
-        //     }
-        //     if(descriptionSummary) {
-        //         form_data.append('descriptionSummary', descriptionSummary);
-        //     }
-        //     if(type) {
-        //         form_data.append('type', type);
-        //     }
-        //     if(isUrgent) {
-        //         form_data.append('isUrgent', isUrgent);
-        //     }
-        //     if(affiliateLinkUrl){
-        //         form_data.append('affiliateLinkUrl', affiliateLinkUrl);
-        //     }
-        //     if(receipts){
-        //         form_data.append('receipts', receipts);
-        //     }
-        //     if(doing_duration){
-        //         form_data.append('doing_duration', doing_duration);
-        //     }
-        //     console.log(form_data);
+            // append datas to a Form Data
+            var form_data = new FormData();
+            if(firstName) {
+                form_data.append('firstName', firstName);
+            }
+            if(lastName) {
+                form_data.append('lastName', lastName);
+            }
+            if(id_type) {
+                form_data.append('id_type', id_type);
+            }
+            if(id_ngo) {
+                form_data.append('id_ngo', id_ngo);
+            }
+            if(birthCertificateNumber) {
+                form_data.append('birthCertificateNumber', birthCertificateNumber);
+            }
+            if(country) {
+                form_data.append('country', country);
+            }
+            if(city) {
+                form_data.append('city', city);
+            }
+            if(idNumber) {
+                form_data.append('idNumber', idNumber);
+            }
+            if(passportNumber){
+                form_data.append('passportNumber', passportNumber);
+            }
+            if(gender){
+                form_data.append('gender', gender);
+            }
+            if(birthDate){
+                form_data.append('birthDate', birthDate);
+            }
+            if(phoneNumber){
+                form_data.append('phoneNumber', phoneNumber);
+            }
+            if(emergencyPhoneNumber){
+                form_data.append('emergencyPhoneNumber', emergencyPhoneNumber);
+            }
+            if(emailAddress){
+                form_data.append('emailAddress', emailAddress);
+            }
+            if(telegramId){
+                form_data.append('telegramId', telegramId);
+            }
+            if(postalAddress){
+                form_data.append('postalAddress', postalAddress);
+            }
+            if(userName){
+                form_data.append('userName', userName);
+            }
+            if(password){
+                form_data.append('password', password);
+            }
+            if(bankAccountNumber){
+                form_data.append('bankAccountNumber', bankAccountNumber);
+            }
+            if(bankAccountShebaNumber){
+                form_data.append('bankAccountShebaNumber', bankAccountShebaNumber);
+            }
+            if(bankAccountCardNumber){
+                form_data.append('bankAccountCardNumber', bankAccountCardNumber);
+            }
+            if(idCardUrl){
+                form_data.append('idCardUrl', idCardUrl);
+            }
+            if(passportUrl){
+                form_data.append('passportUrl', passportUrl);
+            }
+            if(avatarUrl){
+                form_data.append('avatarUrl', avatarUrl);
+            }
+            console.log(form_data);
 
-        //     // update the need with new data in the form
-        //     $.ajax({
-        //         url: SAYApiUrl + '/update/socialWorkerId=' + socialworkerId,
-        //         method: 'PATCH',
-        //         headers : {
-        //             'Access-Control-Allow-Origin'  : baseUrl
-        //         },
-        //         cache: false,
-        //         processData: false,
-        //         contentType: false,
-        //         dataType: 'json',
-        //         data: form_data,
-        //         beforeSend: function(){
-        //             return confirm("You are about to edit the need.\nAre you sure?");
-        //         },
-        //         success: function(data) {
-        //             alert("Success\nNeed updated successfully\n" + JSON.stringify(data.message));
-        //             location.reload();
-        //         },
-        //         error: function(data) {
-        //             bootbox.alert({
-        //                 title: "Error!",
-        //                 message: data.responseJSON.message,
-        //             });
-        //         }
+            // update the need with new data in the form
+            $.ajax({
+                url: SAYApiUrl + '/update/socialWorkerId=' + socialworkerId,
+                method: 'PATCH',
+                headers : {
+                    'Access-Control-Allow-Origin'  : baseUrl
+                },
+                cache: false,
+                processData: false,
+                contentType: false,
+                dataType: 'json',
+                data: form_data,
+                beforeSend: function(){
+                    return confirm("You are about to edit the user.\nAre you sure?");
+                },
+                success: function(data) {
+                    alert("Success\nThe user updated successfully\n" + JSON.stringify(data.message));
+                    location.reload();
+                },
+                error: function(data) {
+                    bootbox.alert({
+                        title: "Error!",
+                        message: data.responseJSON.message,
+                    });
+                }
 
-        //     })  //end of Update ajax
+            })  //end of Update ajax
 
-        // })  //end of 'get the need's data to the form' function
+        })  //end of 'get the need's data to the form' function
 
     })
 
