2e7531568f4687d83f0d6893f85ef2493c0fdd05
elham <elham.ne94@gmail.com>
2020-08-22 15:30:50 +0430

    Undefined need icon while adding need fixed

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,10 @@
 
 // SAY global URL variables
 
-var baseUrl = 'http://192.168.111.2:5001';
+// var baseUrl = 'http://192.168.111.2:5001';
 // var baseUrl = 'http://0.0.0.0:5000';
 // var baseUrl = 'https://master.s.sayapp.company';
-// var baseUrl = 'https://sayapp.company';
+var baseUrl = 'https://sayapp.company';
 var apiEndPointUrl = '/api/v2';
 var SAYApiUrl = baseUrl + apiEndPointUrl;
 // console.log("SAY API url: " + SAYApiUrl);
